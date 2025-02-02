134d2995d7bc6eb0300acf9e2cf448aacbd02594
Amitoshdeep <104307738+Amitoshdeep@users.noreply.github.com>
2025-02-02 16:14:59 +0530

    Add files via upload

---
+++ cpp_files/2024/2/project4.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void setW(){
+	int a;
+	a = 543;
+	cout<<setw(10)<<a<<endl;
+}
+
+void setFill(){
+	int a;
+	a = 543;
+	cout<<setfill('$')<<setw(10)<<a<<endl;
+}
+
+void setPrecision(){
+	float a;
+	a = 543.5647;
+	cout<<fixed <<setfill('*')<<setw(10)<<setprecision(2)<<a<<endl;
+}
+
+void setBase(){
+	int a;
+	a = 543;
+	cout<<setbase(16)<<setfill('*')<<setw(10)<<a<<endl;	
+}
+
+int main(){
+//	cout<<"Anuj Babbar, 2410997052";
+//	cout<<"Ansh Sudan, 2410997047";
+	cout<<"Arshdeep Kaur, 2410997058";
+
+
+//	setW();
+//	setFill();
+//	setPrecision();
+	setBase();
+	return 0;
+}
